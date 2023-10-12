db18e3081a1d8afbcfe31207a56a4a4247ce0f94
Viktor Filippov <jackal3166@gmail.com>
2023-10-13 01:22:53 +0300

    Create SingleItem

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,6 +1,29 @@
-const SingleItem = () => {
-  return (
-    <div>SingleItem</div>
-  )
-}
-export default SingleItem
+import { useState } from 'react';
+
+const SingleItem = ({ item, removeItem }) => {
+	const [isChecked, setIsChecked] = useState(item.completed);
+
+	const handleCheckboxChange = () => {
+		setIsChecked(!isChecked);
+	};
+
+	return (
+		<div className='single-item'>
+			<input
+				type='checkbox'
+				checked={isChecked}
+				onChange={handleCheckboxChange}
+			/>
+			<p
+				style={{
+					textTransform: 'capitalize',
+					textDecoration: isChecked && 'line-through',
+				}}>
+				{item.name}
+			</p>
+			<button className='btn remove-btn'>delete</button>
+		</div>
+	);
+};
+
+export default SingleItem;
